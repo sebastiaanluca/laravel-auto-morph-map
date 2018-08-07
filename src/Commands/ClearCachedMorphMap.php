7e817aa78b981c9050b4b03ada08766ed9640646
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-08-07 18:29:21 +0200

    Add cache commands

---
+++ src/Commands/ClearCachedMorphMap.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SebastiaanLuca\AutoMorphMap\Commands;
+
+use Illuminate\Console\Command;
+use SebastiaanLuca\AutoMorphMap\Mapper;
+
+class ClearCachedMorphMap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'morphmap:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove the morph map cache file';
+
+    /**
+     * @var \SebastiaanLuca\AutoMorphMap\Mapper
+     */
+    private $mapper;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param \SebastiaanLuca\AutoMorphMap\Mapper $mapper
+     */
+    public function __construct(Mapper $mapper)
+    {
+        parent::__construct();
+
+        $this->mapper = $mapper;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle() : void
+    {
+        @unlink($this->mapper->getCachePath());
+
+        $this->info('Morph map models cache cleared!');
+    }
+}
